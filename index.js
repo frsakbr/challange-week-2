cd82ea6ddd888aa5738b2b4a3a9407a3dfff28bd
frsakbr <105800824+frsakbr@users.noreply.github.com>
2022-05-25 16:39:00 +0700

    Menambahkan PUT

--- index.js
+++ index.js
@@ -4,33 +4,35 @@ const app = express();
 const port = 3000;
 
 
-// const fs = require('fs');
-// const rawData = fs.readFileSync("products.json");
-// const products = JSON.parse(rawData);
-
 app.use(express.json());
-app.get('/product',(req, res) => res.send(products));
-app.post('/product', (req, res) => {
-
-    const {body} = req 
-
-    const post = body;
-    products.push(post);
-    res.status(201).send("Berhasil menambahkan Produk")
-})
-app.delete('/product/:id',(req, res) => {
-    products = products.filter(function(i){
-        if (i.id != req.params.id){
-        return i 
-        }
-    })
-    res.status(200).json({
-        message: "Berhasil menghapus Produk"
-    })
-    
+app.get('/', (req, res) => {
+    res.send('Home')
 });
 
+app.get('/product', (req, res) => {
+    res.send(products)
+});
 
-app.listen(port, () => console.log('Example app listening at http'))
+app.post('/product', (req, res) => {
+
+    res.status(201).json(
+        "Berhasil menambahkan Produk"
+    )
+})
+
+app.put('/product', (req, res) => {
+
+    res.status(201).json(
+        "Berhasil mengubah Produk"
+    )
+})
+
+app.delete('/product', (req, res) => {
+
+    res.status(201).json(
+        "Berhasil menghapus Produk"
+    )
+})
 
 
+app.listen(port, () => console.log('Example app listening at port'))
